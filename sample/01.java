a93ebd60181b0cc033740f52512ef0dcf271546d
planetWorldY <51113191+planetWorldY@users.noreply.github.com>
2020-04-12 14:15:35 +0900

    Update 01.java

--- sample/01.java
+++ sample/01.java
@@ -1,1 +1,30 @@
+import javax.swing.*;
+
+public class 01{
+  public static void main(String[] args){
+    JFrame flame = new JFrame("test");
+    flame.seetDefaultCloseOperation(JFlame.EXIT_ON_CLOSE);
+    flame.setSize(600,600);
+    JMenuBar menuBar = new JMenuBar();
+    flame.setJmenuBar(menuBar);
+    JMenu fileMenu = new JMenu("file");
+    menuBar.add(fileMenu);
+      
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
